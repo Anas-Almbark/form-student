b3813eb6025fc7046b97b0bab385e0fdf1a211d4
Anas-Almbark <coder.anas2@gmail.com>
2024-04-15 23:23:34 +0300

    v2

--- app.js
+++ app.js
@@ -1,39 +1,21 @@
 let cards = document.querySelectorAll(".card");
-let form = document.querySelector(".form form");
 let bolts = document.querySelectorAll(".progress ul li");
 let prev = document.querySelector(".prev");
 let next = document.querySelector(".send");
-let inputs = document.querySelectorAll(".card input");
 let currentCardIndex = 0;
 cards[0].classList.add("active");
-
-function animation() {
-  anime({
-    targets: cards[currentCardIndex],
-    translateY: 20,
-  });
-}
-
-prev.addEventListener("click", () => {
-  previousCard();
-});
-next.addEventListener("click", () => {
-  nextCard();
-});
-
+prev.addEventListener("click", () => previousCard());
+next.addEventListener("click", () => nextCard());
 //? for get next card
 function nextCard() {
   if (currentCardIndex < cards.length - 1) {
     currentCardIndex++;
-    cards.forEach((e) => {
-      e.classList.remove("active");
-    });
+    cards.forEach((e) => e.classList.remove("active"));
     cards[currentCardIndex].classList.add("active");
     bolts[currentCardIndex].classList.add("active");
     prev.classList.remove("disabled");
   }
   next.value = currentCardIndex < cards.length - 1 ? "next" : "Send";
-  animation();
 }
 //? for get previous card
 function previousCard() {
@@ -45,5 +27,4 @@ function previousCard() {
   }
   !currentCardIndex && prev.classList.add("disabled");
   next.value = currentCardIndex < cards.length - 1 ? "next" : "Send";
-  animation();
 }
